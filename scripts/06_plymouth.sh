cbdcb5b1f60c2260e0760d834049b9343771cb48
jose risi <jose.risi@gmail.com>
2025-10-13 14:06:12 -0400

    Update 06_plymouth.sh

--- scripts/06_plymouth.sh
+++ scripts/06_plymouth.sh
@@ -3,7 +3,7 @@ echo "🎨 Installing Plymouth theme..."
 sudo apt install -y plymouth plymouth-themes
 THEME_NAME="client-brand"
 THEME_DIR="/usr/share/plymouth/themes/$THEME_NAME"
-THEME_REPO_URL="https://raw.githubusercontent.com/gggoaaat/ubuntuscript/main/assets/plymouth-theme"
+THEME_REPO_URL="https://raw.githubusercontent.com/gggoaaat/ubuntuscript/main/client-brand"
 
 sudo mkdir -p "$THEME_DIR"
 sudo curl -fsSL "$THEME_REPO_URL/$THEME_NAME.plymouth" -o "$THEME_DIR/$THEME_NAME.plymouth"
