2aca9a6b579910f0137cf7ab018e7367bf307d90
jose risi <jose.risi@gmail.com>
2025-10-13 13:32:10 -0400

    Create 06_plymouth.sh

---
+++ scripts/06_plymouth.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "🎨 Installing Plymouth theme..."
+sudo apt install -y plymouth plymouth-themes
+THEME_NAME="client-brand"
+THEME_DIR="/usr/share/plymouth/themes/$THEME_NAME"
+THEME_REPO_URL="https://raw.githubusercontent.com/gggoaaat/ubuntuscript/main/assets/plymouth-theme"
+
+sudo mkdir -p "$THEME_DIR"
+sudo curl -fsSL "$THEME_REPO_URL/$THEME_NAME.plymouth" -o "$THEME_DIR/$THEME_NAME.plymouth"
+sudo curl -fsSL "$THEME_REPO_URL/$THEME_NAME.script" -o "$THEME_DIR/$THEME_NAME.script"
+sudo curl -fsSL "$THEME_REPO_URL/logo.png" -o "$THEME_DIR/logo.png"
+
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth "$THEME_DIR/$THEME_NAME.plymouth" 100
+sudo update-alternatives --set default.plymouth "$THEME_DIR/$THEME_NAME.plymouth"
+sudo update-initramfs -u
